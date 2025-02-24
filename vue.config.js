81fff263350d5255665d4c8be708230bbcee9e5b
jia-niang <1@paperplane.cc>
2025-02-24 10:06:51 +0800

    chore(app): 现在使用 pnpm；现在上传 CDN 的步骤在 CI/CD 中完成

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const S3Plugin = require('webpack-s3-plugin')
 
 module.exports = {
   publicPath: 'https://cdn.paperplane.cc/paperplane-kiny/',
@@ -18,22 +17,5 @@ module.exports = {
         '@css': path.resolve(__dirname, './src/assets/css'),
       },
     },
-
-    plugins: [
-      new S3Plugin({
-        exclude: /.*\.html$/,
-        basePath: 'paperplane-kiny',
-        s3Options: {
-          accessKeyId: process.env.COS_SECRET_ID,
-          secretAccessKey: process.env.COS_SECRET_KEY,
-          region: 'ap-hongkong',
-          endpoint: 'https://cos.ap-hongkong.myqcloud.com',
-          apiVersion: '2006-03-01',
-        },
-        s3UploadOptions: {
-          Bucket: 'paperplane-cdn-1253277322',
-        },
-      }),
-    ],
   },
 }
